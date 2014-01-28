75f10ace59884dc8216737f87fd6f6763e21e315
Spencer McIntyre <zeroSteiner@gmail.com>
2014-01-28 09:30:41 -0500

    Initial commit of python-utilities.

---
+++ utilities/__init__.py
@@ -0,0 +1,302 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  utilities/__init__.py
+#
+#  Redistribution and use in source and binary forms, with or without
+#  modification, are permitted provided that the following conditions are
+#  met:
+#
+#  * Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+#  * Redistributions in binary form must reproduce the above
+#    copyright notice, this list of conditions and the following disclaimer
+#    in the documentation and/or other materials provided with the
+#    distribution.
+#  * Neither the name of the  nor the names of its
+#    contributors may be used to endorse or promote products derived from
+#    this software without specific prior written permission.
+#
+#  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+#  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+#  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+#  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+#  OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+#  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+#  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+#  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+#  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+#  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+#  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+
+import collections
+import functools
+import os
+import time
+
+__version__ = '0.1'
+
+class Cache(object):
+	"""
+	This class provides a simple to use cache object which can be applied
+	as a decorator.
+	"""
+	def __init__(self, timeout):
+		"""
+		@type timeout: string, integer
+		@param timeout: The amount of time in seconds that a cached
+		result will be considered valid for.
+		"""
+		if isinstance(timeout, (str, unicode)):
+			timeout = timedef_to_seconds(timeout)
+		self.cache_timeout = timeout
+		self.__cache = {}
+
+	def __call__(self, *args):
+		if not hasattr(self, '_target_function'):
+			self._target_function = args[0]
+			return self
+		self.cache_clean()
+		if not isinstance(args, collections.Hashable):
+			return self._target_function(*args)
+		result, expiration = self.__cache.get(args, (None, 0))
+		if expiration > time.time():
+			return result
+		result = self._target_function(*args)
+		self.__cache[args] = (result, time.time() + self.cache_timeout)
+		return result
+
+	def __repr__(self):
+		return "<cached function {0}>".format(self._target_function.__name__)
+
+	def cache_clean(self):
+		"""
+		Remove expired items from the cache.
+		"""
+		now = time.time()
+		keys_for_removal = []
+		for key, (value, expiration) in self.__cache.items():
+			if expiration < now:
+				keys_for_removal.append(key)
+		for key in keys_for_removal:
+			del self.__cache[key]
+
+	def cache_clear(self):
+		"""
+		Remove all items from the cache.
+		"""
+		self.__cache = {}
+
+class FileWalker:
+	"""
+	This class is used to easily iterate over files in a directory.
+	"""
+	def __init__(self, filespath, absolute_path = False, skip_files = False, skip_dirs = False, filter_func = None):
+		"""
+		@type filespath: string
+		@param filespath: A path to either a file or a directory.  If a
+		file is passed then that will be the only file returned during the
+		iteration.  If a directory is passed, all files will be recursively
+		returned during the iteration.
+
+		@type absolute_path: boolean
+		@param absolute_path: Whether or not the absolute path or a relative
+		path should be returned.
+
+		@type skip_files: boolean
+		@param skip_files: Whether or not to skip files.
+
+		@type skip_dirs: boolean
+		@param skip_dirs: Whether or not to skip directories.
+
+		@type filter_func: function
+		@param filter_func: If defined, the filter_func function will be called
+		for each file and if the function returns false the file will be
+		skipped.
+		"""
+		if not (os.path.isfile(filespath) or os.path.isdir(filespath)):
+			raise Exception(filespath + ' is neither a file or directory')
+		if absolute_path:
+			self.filespath = os.path.abspath(filespath)
+		else:
+			self.filespath = os.path.relpath(filespath)
+		self.skip_files = skip_files
+		self.skip_dirs = skip_dirs
+		self.filter_func = filter_func
+		if os.path.isdir(self.filespath):
+			self.__iter__= self._next_dir
+		elif os.path.isfile(self.filespath):
+			self.__iter__ = self._next_file
+
+	def _skip(self, cur_file):
+		if self.skip_files and os.path.isfile(cur_file):
+			return True
+		if self.skip_dirs and os.path.isdir(cur_file):
+			return True
+		if self.filter_func != None:
+			if not self.filter_func(cur_file):
+				return True
+		return False
+
+	def _next_dir(self):
+		for root, dirs, files in os.walk(self.filespath):
+			for cur_file in files:
+				cur_file = os.path.join(root, cur_file)
+				if not self._skip(cur_file):
+					yield cur_file
+			for cur_dir in dirs:
+				cur_dir = os.path.join(root, cur_dir)
+				if not self._skip(cur_dir):
+					yield cur_dir
+		raise StopIteration
+
+	def _next_file(self):
+		if not self._skip(self.filespath):
+			yield self.filespath
+		raise StopIteration
+
+class SectionConfigParser(object):
+	"""
+	Proxy access to a section of a ConfigParser object.
+	"""
+	__version__ = '0.1'
+	def __init__(self, section_name, config_parser):
+		"""
+		@type section_name: string
+		@param section_name: Name of the section to proxy access for.
+
+		@type config_parser: ConfigParser.ConfigParser instance
+		@param config_parser: ConfigParser object to proxy access for.
+		"""
+		self.section_name = section_name
+		self.config_parser = config_parser
+
+	def get_raw(self, option, opt_type, default = None):
+		get_func = getattr(self.config_parser, 'get' + opt_type)
+		if default == None:
+			return get_func(self.section_name, option)
+		elif self.config_parser.has_option(self.section_name, option):
+			return get_func(self.section_name, option)
+		else:
+			return default
+
+	def get(self, option, default = None):
+		return self.get_raw(option, '', default)
+
+	def getint(self, option, default = None):
+		return self.get_raw(option, 'int', default)
+
+	def getfloat(self, option, default = None):
+		return self.get_raw(option, 'float', default)
+
+	def getboolean(self, option, default = None):
+		return self.get_raw(option, 'boolean', default)
+
+	def has_option(self, option):
+		return self.config_parser.has_option(self.section_name, option)
+
+	def options(self):
+		return self.config_parser.options(self.section_name)
+
+	def items(self):
+		return self.config_parser.items(self.section_name)
+
+def server_parse(server, default_port):
+	"""
+	Convert a server string to a tuple suitable for passing to connect, for
+	example converting 'www.google.com:443' to ('www.google.com', 443).
+
+	@type server: string
+	@param server: The server string to convert.
+
+	@type default_port: integer
+	@param default_port: The port to use in case one is not specified in
+	the server string.
+	"""
+	server = server.split(':')
+	host = server[0]
+	if len(server) == 1:
+		return (host, default_port)
+	else:
+		port = server[1]
+		if not port:
+			port = default_port
+		else:
+			port = int(port)
+		return (host, port)
+
+def timedef_to_seconds(timedef):
+	"""
+	Convert a string timespan definition to seconds, for example converting
+	'1m30s' to 90.
+
+	@type timedef: string
+	@param timedef: The timespan definition to convert to seconds.
+	"""
+	converter_order = ['w', 'd', 'h', 'm', 's']
+	converters = {
+		'w': 604800,
+		'd': 86400,
+		'h': 3600,
+		'm': 60,
+		's': 1
+	}
+	timedef = timedef.lower()
+	seconds = 0
+	for spec in converter_order:
+		timedef = timedef.split(spec)
+		if len(timedef) == 1:
+			timedef = timedef[0]
+			continue
+		elif len(timedef) > 2 or not timedef[0].isdigit():
+			raise ValueError('invalid time format')
+		adjustment = converters[spec]
+		seconds += (int(timedef[0]) * adjustment)
+		timedef = timedef[1]
+		if not len(timedef):
+			break
+	if timedef.isdigit():
+		seconds += int(timedef)
+	return seconds
+
+def unique(seq, idfunc = None):
+	"""
+	Unique a list or tuple and preserve the order
+
+	@type seq: list, tuple
+	@param seq: The list or tuple to preserve unique items from.
+
+	@type idfunc: Function or None
+	@param idfunc: If idfunc is provided it will be called during the
+	comparison process.
+	"""
+	if idfunc is None:
+		idfunc = lambda x: x
+	preserved_type = type(seq)
+	seen = {}
+	result = []
+	for item in seq:
+		marker = idfunc(item)
+		if marker in seen:
+			continue
+		seen[marker] = 1
+		result.append(item)
+	return preserved_type(result)
+
+def which(program):
+	"""
+	Locate an executable binary's full path by its name.
+
+	@type program: string
+	@param program: The executables name.
+	"""
+	is_exe = lambda fpath: (os.path.isfile(fpath) and os.access(fpath, os.X_OK))
+	if is_exe(program):
+		return program
+	for path in os.environ["PATH"].split(os.pathsep):
+		path = path.strip('"')
+		exe_file = os.path.join(path, program)
+		if is_exe(exe_file):
+			return exe_file
+	return None
---
+++ utilities/job.py
@@ -0,0 +1,351 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  utilities/job.py
+#
+#  Redistribution and use in source and binary forms, with or without
+#  modification, are permitted provided that the following conditions are
+#  met:
+#
+#  * Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+#  * Redistributions in binary form must reproduce the above
+#    copyright notice, this list of conditions and the following disclaimer
+#    in the documentation and/or other materials provided with the
+#    distribution.
+#  * Neither the name of the  nor the names of its
+#    contributors may be used to endorse or promote products derived from
+#    this software without specific prior written permission.
+#
+#  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+#  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+#  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+#  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+#  OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+#  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+#  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+#  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+#  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+#  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+#  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+
+import datetime
+import logging
+import threading
+import time
+import uuid
+
+__version__ = '0.1'
+__all__ = ['JobManager', 'JobRequestDelete']
+
+def normalize_job_id(job_id):
+	if not isinstance(job_id, uuid.UUID):
+		job_id = uuid.UUID(job_id)
+	return job_id
+
+class JobRequestDelete(object):
+	"""
+	An instance of this class can be returned by a job callback to request
+	that the job be deleted and not executed again.
+	"""
+	pass
+
+class JobRun(threading.Thread):
+	def __init__(self, callback, args):
+		self.callback = callback
+		self.callback_args = args
+		self.request_delete = False
+		self.exception = None
+		self.reaped = False
+		threading.Thread.__init__(self)
+
+	def run(self):
+		try:
+			result = self.callback(*self.callback_args)
+			if isinstance(result, JobRequestDelete):
+				self.request_delete = True
+		except Exception as error:
+			self.exception = error
+		return
+
+# Job Dictionary Details:
+#   last_run: datetime.datetime
+#   run_every: datetime.timedelta
+#   job: None or JobRun instance
+#   callback: function
+#   parameters: list of parameters to be passed to the callback function
+#   enabled: boolean if false do not run the job
+#   tolerate_exceptions: boolean if true this job will run again after a failure
+#   run_count: number of times the job has been ran
+#   expiration: number of times to run a job, datetime.timedelta instance or None
+class JobManager(threading.Thread):
+	"""
+	This class provides a threaded job manager for periodically executing
+	arbitrary functions in an asynchronous fashion.
+	"""
+	def __init__(self, use_utc = True):
+		"""
+		@type use_utc: boolean
+		@param use_utc: Whether or not to use UTC time internally.
+		"""
+		super(JobManager, self).__init__()
+		self.__jobs__ = {}
+		self.running = threading.Event()
+		self.shutdown = threading.Event()
+		self.shutdown.set()
+		self.job_lock = threading.RLock()
+		self.use_utc = use_utc
+		self.logger = logging.getLogger(self.__class__.__name__)
+
+	def __job_execute(self, job_id):
+		self.job_lock.acquire()
+		job_desc = self.__jobs__[job_id]
+		job_desc['last_run'] = self.now()
+		job_desc['run_count'] += 1
+		self.logger.debug('executing job with id: ' + str(job_id) + ' and callback function: ' + job_desc['callback'].__name__)
+		job_desc['job'] = JobRun(job_desc['callback'], job_desc['parameters'])
+		job_desc['job'].start()
+		self.job_lock.release()
+
+	def now(self):
+		"""
+		Return a datetime.datetime instance representing the current time.
+		"""
+		if self.use_utc:
+			return datetime.datetime.utcnow()
+		else:
+			return datetime.datetime.now()
+
+	def now_is_after(self, dt):
+		"""
+		Check whether the datetime instance described in d is after the
+		current time.
+
+		@type dt: datetime.datetime instance
+		@param dt: datetime.datetime instance to compare against.
+		"""
+		return bool(dt <= self.now())
+
+	def now_is_before(self, dt):
+		"""
+		Check whether the datetime instance described in d is before the
+		current time.
+
+		@type dt: datetime.datetime instance
+		@param dt: datetime.datetime instance to compare against.
+		"""
+		return bool(dt >= self.now())
+
+	def stop(self):
+		"""
+		Stop the JobManager thread.
+		"""
+		self.logger.debug('stopping the job manager')
+		self.running.clear()
+		self.shutdown.wait()
+		self.job_lock.acquire()
+		self.logger.debug('waiting on ' + str(len(self.__jobs__)) + ' job threads')
+		for job_id, job_desc in self.__jobs__.items():
+			if job_desc['job'] == None:
+				continue
+			if not job_desc['job'].is_alive():
+				continue
+			job_desc['job'].join()
+		self.join()
+		self.job_lock.release()
+		self.logger.info('the job manager has been stopped')
+		return
+
+	def run(self):
+		self.logger.info('the job manager has been started')
+		self.running.set()
+		self.shutdown.clear()
+		self.job_lock.acquire()
+		while self.running.is_set():
+			self.job_lock.release()
+			time.sleep(1)
+			self.job_lock.acquire()
+			if not self.running.is_set():
+				break
+
+			# Reap Jobs
+			jobs_for_removal = []
+			for job_id, job_desc in self.__jobs__.items():
+				job_obj = job_desc['job']
+				if job_obj.is_alive() or job_obj.reaped:
+					continue
+				if job_obj.exception != None:
+					if job_desc['tolerate_exceptions'] == False:
+						self.logger.error('job ' + str(job_id) + ' encountered an error and is not set to tolerate exceptions')
+						jobs_for_removal.append(job_id)
+					else:
+						self.logger.warning('job ' + str(job_id) + ' encountered exception: ' + job_obj.exception.__class__.__name__)
+				if isinstance(job_desc['expiration'], int):
+					if job_desc['expiration'] <= 0:
+						jobs_for_removal.append(job_id)
+					else:
+						job_desc['expiration'] -= 1
+				elif isinstance(job_desc['expiration'], datetime.datetime):
+					if self.now_is_after(job_desc['expiration']):
+						jobs_for_removal.append(job_id)
+				if job_obj.request_delete:
+					jobs_for_removal.append(job_id)
+				job_obj.reaped = True
+			for job_id in jobs_for_removal:
+				self.job_delete(job_id)
+
+			# Sow Jobs
+			for job_id, job_desc in self.__jobs__.items():
+				if job_desc['last_run'] != None and self.now_is_before(job_desc['last_run'] + job_desc['run_every']):
+					continue
+				if job_desc['job'].is_alive():
+					continue
+				if not job_desc['job'].reaped:
+					continue
+				if not job_desc['enabled']:
+					continue
+				self.__job_execute(job_id)
+		self.job_lock.release()
+		self.shutdown.set()
+
+	def job_run(self, callback, parameters = []):
+		"""
+		Add a job and run it once immediately.
+
+		@type callback: function
+		@param callback: The function to run asynchronously.
+
+		@type parameters: list, tuple
+		@param parameters: The parameters to be provided to the callback.
+		"""
+		if not isinstance(parameters, (list, tuple)):
+			parameters = (parameters,)
+		job_desc = {}
+		job_desc['job'] = JobRun(callback, parameters)
+		job_desc['last_run'] = None
+		job_desc['run_every'] = datetime.timedelta(0, 1)
+		job_desc['callback'] = callback
+		job_desc['parameters'] = parameters
+		job_desc['enabled'] = True
+		job_desc['tolerate_exceptions'] = False
+		job_desc['run_count'] = 0
+		job_desc['expiration'] = 0
+		job_id = uuid.uuid4()
+		self.logger.info('adding new job with id: ' + str(job_id) + ' and callback function: ' + callback.__name__)
+		with self.job_lock:
+			self.__jobs__[job_id] = job_desc
+			self.__job_execute(job_id)
+		return job_id
+
+	def job_add(self, callback, parameters = [], hours = 0, minutes = 0, seconds = 0, tolerate_exceptions = True, expiration = None):
+		"""
+		Add a job to the job manager.
+
+		@type callback: function
+		@param callback: The function to run asynchronously.
+
+		@type parameters: list, tuple
+		@param parameters: The parameters to be provided to the callback.
+
+		@type hours: integer
+		@param hours: Number of hours to sleep between running the callback.
+
+		@type minutes: integer
+		@param minutes: Number of minutes to sleep between running the callback.
+
+		@type seconds: integer
+		@param seconds: Number of seconds to sleep between running the callback.
+
+		@type tolerate_exceptions: boolean
+		@param tolerate_execptions: Whether to continue running a job after
+		it has thrown an exception.
+
+		@type expiration: integer, datetime.timedelta instance, datetime.datetime instance
+		@param expiration: When to expire and remove the job. If an integer is
+		provided, the job will be executed that many times.  If a datetime or
+		timedelta instance is provided, then the job will be removed after the
+		specified time.
+		"""
+		if not isinstance(parameters, (list, tuple)):
+			parameters = (parameters,)
+		job_desc = {}
+		job_desc['job'] = JobRun(callback, parameters)
+		job_desc['last_run'] = None
+		job_desc['run_every'] = datetime.timedelta(0, ((hours * 60 * 60) + (minutes * 60) + seconds))
+		job_desc['callback'] = callback
+		job_desc['parameters'] = parameters
+		job_desc['enabled'] = True
+		job_desc['tolerate_exceptions'] = tolerate_exceptions
+		job_desc['run_count'] = 0
+		if isinstance(expiration, int):
+			job_desc['expiration'] = expiration
+		elif isinstance(expiration, datetime.timedelta):
+			job_desc['expiration'] = self.now() + expiration
+		elif isinstance(expiration, datetime.datetime):
+			job_desc['expiration'] = expiration
+		else:
+			job_desc['expiration'] = None
+		job_id = uuid.uuid4()
+		self.logger.info('adding new job with id: ' + str(job_id) + ' and callback function: ' + callback.__name__)
+		with self.job_lock:
+			self.__jobs__[job_id] = job_desc
+		return job_id
+
+	def job_count(self):
+		"""
+		Return the number of jobs.
+		"""
+		return len(self.__jobs__)
+
+	def job_count_enabled(self):
+		"""
+		Return the number of enabled jobs.
+		"""
+		return len(filter(lambda job_desc: job_desc['enabled'], self.__jobs__.values()))
+
+	def job_enable(self, job_id):
+		"""
+		Enable a job.
+
+		@type job_id: uuid.UUID
+		@param job_id: Job identifier to enable.
+		"""
+		job_id = normalize_job_id(job_id)
+		with self.job_lock:
+			job_desc = self.__jobs__[job_id]
+			job_desc['enabled'] = True
+
+	def job_disable(self, job_id):
+		"""
+		Disable a job.
+
+		@type job_id: uuid.UUID
+		@param job_id: Job identifier to disable.
+		"""
+		job_id = normalize_job_id(job_id)
+		with self.job_lock:
+			job_desc = self.__jobs__[job_id]
+			job_desc['enabled'] = False
+
+	def job_delete(self, job_id):
+		"""
+		Delete a job.
+
+		@type job_id: uuid.UUID
+		@param job_id: Job identifier to delete.
+		"""
+		job_id = normalize_job_id(job_id)
+		self.logger.info('deleting job with id: ' + str(job_id) + ' and callback function: ' + self.__jobs__[job_id]['callback'].__name__)
+		with self.job_lock:
+			del self.__jobs__[job_id]
+
+	def job_exists(self, job_id):
+		"""
+		Check if a job identifier exists.
+
+		@type job_id: uuid.UUID
+		@param job_id: Job identifier to check.
+		"""
+		job_id = normalize_job_id(job_id)
+		return job_id in self.__jobs__
