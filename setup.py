f8edceb2ec828fbc179514492e147e868f75b16c
Spencer McIntyre <zeroSteiner@gmail.com>
2018-10-09 15:58:22 -0700

    Bump the version to 1.3.5

--- setup.py
+++ setup.py
@@ -56,7 +56,7 @@ This project provides a collection of miscellaneous Python utilities.\
 
 setup(
 	name='smoke-zephyr',
-	version='1.3.4',
+	version='1.3.5',
 	author='Spencer McIntyre',
 	author_email='zeroSteiner@gmail.com',
 	maintainer='Spencer McIntyre',
--- smoke_zephyr/__init__.py
+++ smoke_zephyr/__init__.py
@@ -33,7 +33,7 @@
 import collections
 
 # Semantic Versioning: http://semver.org/spec/v2.0.0.html
-version_info = collections.namedtuple('version_info', ['major', 'minor', 'micro'])(1, 3, 4)
+version_info = collections.namedtuple('version_info', ['major', 'minor', 'micro'])(1, 3, 5)
 """A tuple representing the version information in the format ('major', 'minor', 'micro')"""
 
 version_label = ''
